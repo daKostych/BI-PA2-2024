92dae6caaf835de6489fa25cfc9540e1b5d358cd
Kanstantsin Downar <downakan@fit.cvut.cz>
2024-04-20 00:07:08 +0200

    Declaration of CPos class

--- Progtest/FITexcel/position.h
+++ Progtest/FITexcel/position.h
@@ -1,8 +1,15 @@
+#include <string>
+
+using namespace std;
+
+//======================================================================================================================
 class CPos
 {
 public:
-    CPos(std::string_view str);
+    CPos() = default;
+    explicit CPos(string_view str);
+    friend bool operator<(const CPos & lhs, const CPos & rhs);
 
-private:
-    unsigned _row, _column;
+    unsigned _row = 0, _column = 0;
 };
+//======================================================================================================================
