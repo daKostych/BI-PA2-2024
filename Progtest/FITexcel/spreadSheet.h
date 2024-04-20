bf9c9408ff42ee2b0745c9c83c9c07a9a973e50b
Kanstantsin Downar <downakan@fit.cvut.cz>
2024-04-20 22:43:21 +0200

    Add map with origin strings and all save/load related methods

--- Progtest/FITexcel/spreadSheet.h
+++ Progtest/FITexcel/spreadSheet.h
@@ -27,13 +27,15 @@ public:
     CSpreadsheet & operator=(const CSpreadsheet & other);
     CSpreadsheet & operator=(CSpreadsheet && other) noexcept;
 
-    //bool load(std::istream & is);
-    //bool save(std::ostream & os) const;
+    bool load(std::istream & is);
+    static bool loadSell(const string & cell, CSpreadsheet & spreadSheet);
+    bool save(std::ostream & os) const;
     bool setCell(CPos pos, std::string contents);
     CValue getValue(CPos pos);
     //void copyRect(CPos dst, CPos src, int w = 1, int h = 1);
 
 private:
     map<CPos, ANode> _table;
+    map<CPos, string> _stringTable;
 };
 //======================================================================================================================
