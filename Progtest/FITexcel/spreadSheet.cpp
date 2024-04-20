9fcf3fc7a244bb8c4085b7d3fe09feea3985a70f
Kanstantsin Downar <downakan@fit.cvut.cz>
2024-04-20 22:57:10 +0200

    Implementation of the save/load related methods

--- Progtest/FITexcel/spreadSheet.cpp
+++ Progtest/FITexcel/spreadSheet.cpp
@@ -1,4 +1,5 @@
 #include "spreadSheet.h"
+#include <iostream>
 
 //======================================================================================================================
 CSpreadsheet::CSpreadsheet(const CSpreadsheet & other)
@@ -6,9 +7,16 @@ CSpreadsheet::CSpreadsheet(const CSpreadsheet & other)
     _table.clear();
     for (const auto & element : other._table)
         _table.emplace(element.first, element.second->cloneNode());
+    _stringTable.clear();
+    for (const auto & element : other._stringTable)
+        _stringTable[element.first] = element.second;
 }
 //======================================================================================================================
-CSpreadsheet::CSpreadsheet(CSpreadsheet && other) noexcept { _table = std::move(other._table); }
+CSpreadsheet::CSpreadsheet(CSpreadsheet && other) noexcept
+{
+    _table = std::move(other._table);
+    _stringTable = std::move(other._stringTable);
+}
 //======================================================================================================================
 CSpreadsheet & CSpreadsheet::operator=(const CSpreadsheet & other)
 {
@@ -17,6 +25,7 @@ CSpreadsheet & CSpreadsheet::operator=(const CSpreadsheet & other)
 
     CSpreadsheet copyToSwap(other);
     swap(_table, copyToSwap._table);
+    swap(_stringTable, copyToSwap._stringTable);
     return *this;
 }
 //======================================================================================================================
@@ -26,20 +35,86 @@ CSpreadsheet & CSpreadsheet::operator=(CSpreadsheet && other) noexcept
         return *this;
 
     _table = std::move(other._table);
+    _stringTable = std::move(other._stringTable);
     return *this;
 }
 //======================================================================================================================
-//bool CSpreadsheet::load(std::istream & is) {} // todo
+bool CSpreadsheet::loadSell(const string & cell, CSpreadsheet & spreadSheet)
+{
+    string column, row ,value;
+    if (cell[0] != 'c')
+        return false;
+
+    size_t index = 1;
+    for (; index < cell.size() && cell[index] != 'r'; index++)
+    {
+        if (cell[index] < '0' || cell[index] > '9')
+            return false;
+        column += cell[index];
+    }
+    index++;
+    for (; index < cell.size() && cell[index] != '_'; index++)
+    {
+        if (cell[index] < '0' || cell[index] > '9')
+            return false;
+        row += cell[index];
+    }
+    index++;
+    for (; index < cell.size(); index++)
+        value += cell[index];
+
+    try
+    {
+        spreadSheet.setCell(CPos(stoul(column), stoul(row)), value);
+        return true;
+    }
+    catch (const exception & ex ) { return false; }
+}
 //======================================================================================================================
-//bool CSpreadsheet::save(std::ostream & os) const {} // todo
+bool CSpreadsheet::load(std::istream & is)
+{
+    CSpreadsheet newSpreadSheet;
+    string fullTable, tablePart;
+    while (getline(is, tablePart))
+        fullTable += tablePart;
+    for(size_t i = 0, from = 0; i < fullTable.size(); i++)
+    {
+        if (fullTable[i] == '|')
+        {
+            if (!loadSell(fullTable.substr(from, i - from), newSpreadSheet))
+                return false;
+            from = i + 1;
+        }
+    }
+    _table = std::move(newSpreadSheet._table);
+    _stringTable = std::move(newSpreadSheet._stringTable);
+    return true;
+}
+//======================================================================================================================
+bool CSpreadsheet::save(std::ostream & os) const
+{
+    string fullTable;
+    for (const auto & element : _stringTable)
+        fullTable += "c" + to_string(element.first._column) +
+                     "r" + to_string(element.first._row) +
+                     "_" + element.second + '|';
+    if (!fullTable.empty())
+        fullTable.erase(fullTable.size() - 1);
+    else
+        return false;
+
+    os << fullTable;
+    return true;
+}
 //======================================================================================================================
 bool CSpreadsheet::setCell(CPos pos, std::string contents)
 {
     try
     {
         Builder builder;
-        parseExpression(std::move(contents), builder);
+        parseExpression(contents, builder);
         _table[pos] = std::move(builder.builderStack.top());
+        _stringTable[pos] = std::move(contents);
     }
     catch (const exception & ex)
     {
