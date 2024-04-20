f66276504c9bd5f3114d9ae06b2cfe25a246dbbf
Kanstantsin Downar <downakan@fit.cvut.cz>
2024-04-20 22:38:44 +0200

    Add one more constructor

--- Progtest/FITexcel/position.h
+++ Progtest/FITexcel/position.h
@@ -10,6 +10,7 @@ class CPos
 public:
     CPos() = default;
     explicit CPos(string_view str);
+    explicit CPos(unsigned column, unsigned row) : _row(row), _column(column) {}
     friend bool operator<(const CPos & lhs, const CPos & rhs);
 
     unsigned _row = 0, _column = 0;
